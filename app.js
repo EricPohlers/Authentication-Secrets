e21ea1b80e5b79224edd50e79a8cd9512fb792e0
Eric Pohlers <e.pohlers@web.de>
2020-05-11 11:29:38 +0200

    add evironment vars

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
@@ -20,9 +21,10 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-const secret = 'Thisisourlittlesecret';
-
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ['password'],
+});
 
 const User = new mongoose.model('User', userSchema);
 
